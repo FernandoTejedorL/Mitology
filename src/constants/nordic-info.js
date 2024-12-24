0ff36093ae31e3548be7feb2c6c29d1f2f79a866
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-24 15:03:25 +0100

    update

---
+++ src/constants/nordic-info.js
@@ -0,0 +1,102 @@
+import { v4 } from 'uuid';
+import { COLORS } from '../styles/colors';
+
+export const NORDIC_INFO = [
+	{
+		id: v4(),
+		color: COLORS,
+		banner: {
+			mobile: '',
+			tablet: '',
+			desktop: ''
+		},
+		kind: '',
+		0: {
+			beingOne: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingTwo: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingThree: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			}
+		},
+		1: {
+			beingOne: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingTwo: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingThree: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			}
+		},
+		2: {
+			beingOne: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingTwo: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			},
+			beingThree: {
+				name: '',
+				image: {
+					mobile: '',
+					tablet: '',
+					desktop: ''
+				},
+				caption: ''
+			}
+		}
+	}
+];
